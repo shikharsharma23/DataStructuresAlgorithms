6e5c5209cb3e155ef54af9daff35cd859a0bf706
Shikhar Sharma <shikharcic23@gmail.com>
2021-02-20 19:19:43 +0530

    Indention fixes for Linked List Intersection

--- LinkedListIntersection.py
+++ LinkedListIntersection.py
@@ -1,8 +1,10 @@
 # Definition for singly-linked list.
- class ListNode:
+class ListNode:
      def __init__(self, x):
-         self.val = x
-         self.next = None
+        self.val = x
+        self.next = None
+
+
 
 class Solution:
     def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> ListNode:
